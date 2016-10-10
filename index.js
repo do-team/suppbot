dea5d9f31c56b82f383fd6fe5c0cf50167735c42
David Kubec <davesade.42@gmail.com>
2016-10-10 18:33:51 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+var mysql = require('mysql');
+
+exports.handler = function(event, context) {
+
+    if (event === null || event.text === null) {
+        context.succeed('You sent nothing!');
+    }
+    else 
+    {
+        if(event.text.toLowerCase().indexOf('david') !== -1){
+            context.succeed(' hello ' + event.text);
+        }  
+        if(event.text.toLowerCase().indexOf('db') !== -1){
+            
+           
+           var connection =  mysql.createConnection({
+          host   : 'futuredb.cbhsjvpjrptr.us-west-2.rds.amazonaws.com',
+          user   : 'marty',
+          password : 'martymarty',
+          database : 'Member'
+        });
+           
+            
+            connection.connect(function(err){
+                if(err){
+                    context.fail('Database connection failed' + err);
+                }    
+                else{
+                connection.query("SELECT business_id FROM members WHERE member_id='MMXAM'",function(err,rows,fields){
+                        if(err)
+                        {
+                           context.fail('Cannot read from database!' + err); 
+                        }
+                        else
+                        {
+                            context.succeed(rows[0]);
+                            connection.end();
+                        }
+                    });
+                }
+            });
+        }  
+        else
+        {
+           context.succeed('You sent: ' + event.text);
+        }
+    }
+};
